96836dbf1d9774b7040b10ee09a33aa4ea57f544
BhaswatiNath <nathbhaswati321@gmail.com>
2025-07-18 00:13:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import joblib
 
-# Load the model
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
 st.title("Emotion Detection App")
 
-# User input
 user_input = st.text_input("Enter your text:")
 
 if user_input:
